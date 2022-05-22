7470113ce8cb72ae0c6dfb7925e94fd1b2799c70
Eshaan Aggarwal <eshaan.aggarwal.cse21@itbhu.ac.in>
2022-05-22 15:27:24 +0530

    Add NameCard

--- utils/contributors.js
+++ utils/contributors.js
@@ -59,6 +59,32 @@ const CONTRIBUTERS = [
         `,
         github: "https://github.com/debarati-06",
     },
+    {
+        handle_name: 'EshaanAgg',
+        name: "Eshaan Aggarwal",
+        image: "/images/EshaanAgg.jpg",
+        description: `
+        <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+        <div class="text-center">
+            <lord-icon src="https://cdn.lordicon.com/uxtcmjmu.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>     
+                Caffenating 
+            <lord-icon src="https://cdn.lordicon.com/uxtcmjmu.json" trigger="loop" colors="primary:#121331,secondary:#08a88a" style="width:25px;height:25px"></lord-icon>     
+            <br />
+            <lord-icon src="https://cdn.lordicon.com/wxnxiano.json" trigger="loop" colors="primary:#121331,secondary:#2516c7" style="width:25px;height:25px"></lord-icon>
+                Working 
+            <lord-icon src="https://cdn.lordicon.com/wxnxiano.json" trigger="loop" colors="primary:#121331,secondary:#2516c7" style="width:25px;height:25px"></lord-icon>
+            <br />
+            <lord-icon src="https://cdn.lordicon.com/anihkfxp.json" trigger="loop" style="width:32px;height:32px"></lord-icon>
+                Using 
+            <lord-icon src="https://cdn.lordicon.com/anihkfxp.json" trigger="loop" style="width:32px;height:32px"></lord-icon>
+            <br />
+            <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#e88c30" style="width:25px;height:25px"></lord-icon>
+                Eating 
+            <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#e88c30" style="width:25px;height:25px"></lord-icon>
+        </div>
+    `,
+        github: "https://github.com/EshaanAgg",
+    }
 ];
 
 export default CONTRIBUTERS;
