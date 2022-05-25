cd9dc810f48c547202ceae836f735cca9e42c976
SaiRev <saiyamjain1234567@gmail.com>
2022-05-26 03:16:39 +0530

    Saiyam name Card

--- utils/contributors.js
+++ utils/contributors.js
@@ -5,8 +5,32 @@ const CONTRIBUTERS = [
         image: "/images/cops.jpg",
         description: "Hello everyone, this is the first card. Go ahead, add your own!",
         github: "https://github.com/COPS-IITBHU",
+    },{
+        handle_name: 'SaiRev0',
+        name: "Saiyam Jain",
+        image: "/images/saiyam.png",
+        description: `
+            <script src="https://cdn.lordicon.com//libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+            <div class="text-center">
+               <lord-icon src="https://cdn.lordicon.com/ycnmpwon.json" trigger="loop" colors="primary:#121331,secondary:#66d7ee" style="width:25px;height:25px"></lord-icon>    
+                    Comding
+                <lord-icon src="https://cdn.lordicon.com/ycnmpwon.json" trigger="loop" colors="primary:#121331,secondary:#66d7ee" style="width:25px;height:25px"></lord-icon>    
+                <br />
+                <lord-icon src="https://cdn.lordicon.com/wxnxiano.json" trigger="loop" colors="primary:#121331,secondary:#2516c7" style="width:25px;height:25px"></lord-icon>
+                    Assignments
+                <lord-icon src="https://cdn.lordicon.com/wxnxiano.json" trigger="loop" colors="primary:#121331,secondary:#2516c7" style="width:25px;height:25px"></lord-icon>
+                    <br />
+                <lord-icon src="https://cdn.lordicon.com/fvkdonkw.json" trigger="loop" colors="primary:#121331,secondary:#c7166f" style="width:25px;height:25px"></lord-icon>
+                    Valorant
+                <lord-icon src="https://cdn.lordicon.com/fvkdonkw.json" trigger="loop" colors="primary:#121331,secondary:#c7166f" style="width:25px;height:25px"></lord-icon>
+                <br />
+                <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#e88c30" style="width:25px;height:25px"></lord-icon>
+                    Eating
+                    <lord-icon src="https://cdn.lordicon.com/jpdtnwas.json" trigger="loop" colors="primary:#121331,secondary:#e88c30" style="width:25px;height:25px"></lord-icon>
+            </div>
+        `,
+        github: "https://github.com/SaiRev0",
     },
-    // Start adding your data below
     {
         handle_name: 'ankur12-160',
         name: "Ankur Patil",
